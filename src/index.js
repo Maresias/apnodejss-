9d841b5ff0e824de78b2de4775d1e81c535133c6
Maresias <ale.maresias@gmail.com>
2025-08-27 22:59:07 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,39 +1,70 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) =>{
-    res.send('Hello World!')
-})
+const dados = []
+
+
+function middleware (req, res, next){
+    const {method, url} = req
+    const router = `[${method}] ${url}`
+    console.log(router)
+    next()
+}
+
+app.use(middleware)
 
 app.get('/projects', (req, res) =>{
-    const {name, age} = req.query
-    res.send(`${name} has ${age} years`)
+    return res.json(dados)
 })
 
 app.post('/projects', (req, res)=>{
     const { name, age } = req.body
-    res.send([
-        `name: ${name}, age: ${age}`
-    ])
+    
+    const dado = {
+        id: uuidv4(),
+        name,
+        age
+    }
+
+    dados.push(dado)
+    return res.sendStatus(201)
 })
 
 app.put('/projects/:id', (req, res) => {
     const { id } = req.params
     const { age, name } = req.body
-    res.send(`{
-        "id": ${id},
-        "age": ${age},
-        "name": "${name}"
-    }`)
+    
+    const index = dados.findIndex(dado => dado.id === id)
+
+    if (index < 0){
+        return res.json({error: 'Dado not found'}).status(404)
+    }
+    if (!name || !age){
+        return res.json({error: 'Name and age are required'}).status(400)
+    }
+
+    dados[index] = {
+        id,
+        name,
+        age
+    }
+
+    return res.json(dados[index]).status(204)
 })
 
 app.delete('/projects/:id', (req, res) => {
+    const {id } = req.params
+    const index = dados.findIndex(dado => dado.id === id)
 
-    res.send(`{
-        "zerado"
-    }`)
+    if (index < 0){
+        return res.json({error: 'Dado not found'}).status(404)
+    }
+
+    dados.splice(index, 1)
+    return res.sendStatus(204)
 })
 
 app.listen(3000, () =>{
